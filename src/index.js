e4385e99e0302f8c956c2e1bf06fe02ec58821a9
181221 <peder.wiig@gmail.com>
2019-07-02 21:35:19 +0200

    add prisma

--- src/index.js
+++ src/index.js
@@ -6,33 +6,43 @@ let links = [
     description: "Fullstack tutorial for GraphQL"
   }
 ];
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
 
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: () => links,
+    link: () => `This is the API of a Hackernews Clone`
   },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
+  Mutation: {
+    post: (parent, { description, url }) => {
+      const link = {
+        id: `link-${idCount}`,
+        description: description,
+        url: url
+      };
+      links.push(link);
+      idCount++;
+      return link;
+    },
+    deleteLink: (parent, { id }) => {
+      links = links.filter(el => el.id !== id);
+      return id;
+    },
+    updateLink: (parent, { id, url, description }) => {
+      links.map(el => {
+        if (el.id === id) {
+          el.url = url ? url : el.url;
+          el.description = description ? description : el.description;
+        }
+      });
+      return "you updated link lol ";
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
